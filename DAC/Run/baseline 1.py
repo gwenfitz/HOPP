e960d60a2030de4c0935013b84902d0daeefd1c2
FitzSimmons <gfitzsim@nrel.gov>
2024-07-08 09:30:12 -0600

    Update 6/8

--- DAC/Run/baseline 1.py
+++ DAC/Run/baseline 1.py
@@ -3,77 +3,82 @@ import numpy as np
 from hopp.utilities import load_yaml
 from hopp.simulation.technologies.sites import SiteInfo
 from hopp.simulation import HoppInterface
+import floris.tools.visualization as wakeviz
+import matplotlib.pyplot as plt
 
 import greenheart.simulation.technologies.dac.direct_air_capture as dac
 
-def runsimulation(showplots == false)
-#load floris config
-floris_config = load_yaml("./DAC/Inputs/gch_dacdisk_turbine.yaml")
+def runsimulation(showplots = 'false'):
+    
+    
+    #load floris config
+    # floris_config = load_yaml("./Inputs/gch_dacdisk_turbine.yaml")
+    floris_config = load_yaml("HOPP/DAC/Inputs/gch_dacdisk_turbine.yaml")
 
-# load base hopp config
-hopp_config = load_yaml("./DAC/Inputs/dac_hopp.yaml")
+    # load base hopp config
+    hopp_config = load_yaml("HOPP/DAC/Inputs/dac_hopp.yaml")
 
-# load hopp site
-hopp_site = SiteInfo(**hopp_config["site"])
-wind_data = hopp_site.wind_resource._data["data"]
-wind_speed = [W[2] for W in wind_data]        # at 10m height, could use equation to find at diff heights.
+    # load hopp site
+    hopp_site = SiteInfo(**hopp_config["site"])
+    wind_data = hopp_site.wind_resource._data["data"]
+    wind_speed = [W[2] for W in wind_data]        # at 10m height, could use equation to find at diff heights.
 
 
-dac_disk = floris_config['farm']['turbine_type'][0] # Single disk of DAC stack
-turb = floris_config['farm']['turbine_type'][1]  # Wind turbine model
+    dac_disk = floris_config['farm']['turbine_type'][0] # Single disk of DAC stack
+    turb = floris_config['farm']['turbine_type'][1]  # Wind turbine model
 
-# print(dac_disk)
+    # print(dac_disk)
 
-# Now, set up the x, y locations for each of the DACs (two rows of four DACS)
-dacs_layout_x = np.array([0, 500, 1000, 1500, 0, 500, 1000, 1500])
-dacs_layout_y = np.array([0, 0, 0, 0, 500, 500, 500, 500])
-n_dacs = len(dacs_layout_x)
+    # Now, set up the x, y locations for each of the DACs (two rows of four DACS)
+    dacs_layout_x = np.array([0, 500, 1000, 1500, 0, 500, 1000, 1500])
+    dacs_layout_y = np.array([0, 0, 0, 0, 500, 500, 500, 500])
+    n_dacs = len(dacs_layout_x)
 
-# Generate a list of the "hub heights" of the DAC disks in a single DAC
-dac_heights = [2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5]
+    # Generate a list of the "hub heights" of the DAC disks in a single DAC
+    dac_heights = [2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5]
 
-# # Generate a list of turbine definitions that specify a single DAC
-dac_stack = []
-for i, hub_height in enumerate(dac_heights): # Loop over the hub heights
-    dac_disk_i = dac_disk.copy() # create a copy of the dictionary
-    dac_disk_i["hub_height"] = hub_height
-    # Need to rename to avoid a bug
-    dac_disk_i["turbine_type"] = "dac-disk-"+str(hub_height)
-    dac_stack.append(dac_disk_i)
+    # # Generate a list of turbine definitions that specify a single DAC
+    dac_stack = []
+    for i, hub_height in enumerate(dac_heights): # Loop over the hub heights
+        dac_disk_i = dac_disk.copy() # create a copy of the dictionary
+        dac_disk_i["hub_height"] = hub_height
+        # Need to rename to avoid a bug
+        dac_disk_i["turbine_type"] = "dac-disk-"+str(hub_height)
+        dac_stack.append(dac_disk_i)
 
-# Tile this list to match the number of DACs (8, in this case)
-dacs_all = dac_stack * n_dacs
+    # Tile this list to match the number of DACs (8, in this case)
+    dacs_all = dac_stack * n_dacs
 
-# We'll need to extend the  (x,y) layouts to match the number of hub heights
-dacs_layout_x = np.repeat(dacs_layout_x, len(dac_heights)).tolist()
-dacs_layout_y = np.repeat(dacs_layout_y, len(dac_heights)).tolist()
+    # We'll need to extend the  (x,y) layouts to match the number of hub heights
+    dacs_layout_x = np.repeat(dacs_layout_x, len(dac_heights)).tolist()
+    dacs_layout_y = np.repeat(dacs_layout_y, len(dac_heights)).tolist()
 
-# Check it worked... expect 2, 2.5, 4.5, 2 for the hub heights,
-# at the (0,0) DAC and then the (500,0) DAC
-print("Some DAC locations before assignment (x, y, height):")
-for i in [0, 1, 5, 6]:
-    print(dacs_layout_x[i], dacs_layout_y[i], dacs_all[i]["hub_height"])
+    # Check it worked... expect 2, 2.5, 4.5, 2 for the hub heights,
+    # at the (0,0) DAC and then the (500,0) DAC
+    print("Some DAC locations before assignment (x, y, height):")
+    for i in [0, 1, 5, 6]:
+        print(dacs_layout_x[i], dacs_layout_y[i], dacs_all[i]["hub_height"])
 
-# Now, we'll create the layout and list of actual wind turbines
-turbs_layout_x = [2000, 2000, 4000, 4000]
-turbs_layout_y = [0, 2000, 0, 2000]
-turbs_all = [turb] * len(turbs_layout_x) # List of 4 turbines
+    # Now, we'll create the layout and list of actual wind turbines
+    turbs_layout_x = [2000, 2000, 4000, 4000]
+    turbs_layout_y = [0, 2000, 0, 2000]
+    turbs_all = [turb] * len(turbs_layout_x) # List of 4 turbines
 
-# Join the layouts of DACs and turbines, as well as their dictionary lists
-layout_x = dacs_layout_x + turbs_layout_x
-layout_y = dacs_layout_y + turbs_layout_y
-type_defs = dacs_all + turbs_all
+    # Join the layouts of DACs and turbines, as well as their dictionary lists
+    layout_x = dacs_layout_x + turbs_layout_x
+    layout_y = dacs_layout_y + turbs_layout_y
+    type_defs = dacs_all + turbs_all
 
-# update floris config definitions
-floris_config["farm"]["layout_x"] = layout_x
-floris_config["farm"]["layout_y"] = layout_y
-floris_config["farm"]["turbine_type"] = type_defs
+    # update floris config definitions
+    floris_config["farm"]["layout_x"] = layout_x
+    floris_config["farm"]["layout_y"] = layout_y
+    floris_config["farm"]["turbine_type"] = type_defs
 
-# load floris config into hopp config
-hopp_config["technologies"]["wind"]["floris_config"] = floris_config
+    # load floris config into hopp config
+    hopp_config["technologies"]["wind"]["floris_config"] = floris_config
 
-#run DAC
-co2, _, power = dac.driver(wind_speed) # 
+    #run DAC
+    co2, power = dac.driver(wind_speed,8) # 
 
 ## WIND SPEED: 8760 list of wind speed at 10m. Could make a 2D array or repeatedly call
 # change wind profile at each module height down from 10 meters using equation
@@ -86,22 +91,32 @@ co2, _, power = dac.driver(wind_speed) #
 # From driver return module CO2 produced (array), stack CO2 (1D list), total DAC farm CO2 produced (stack * number of DAC units)
 
 
-# # battery requirements
-dac_power_requirements = [x / 1000 for x in power] #[MW]
-hopp_config["site"]["desired_schedule"] = dac_power_requirements
+    # # battery requirements
+    dac_power_requirements = [x / 1000 for x in power] #[MW]
+    hopp_config["site"]["desired_schedule"] = dac_power_requirements
 
-# load hopp interface
-hi = HoppInterface(hopp_config)
-print(hi.system.site.wind_resource)
+    # load hopp interface
+    hi = HoppInterface(hopp_config)
+    print(hi.system.site.wind_resource)
 
-# run hopp simulation
-hi.simulate(project_life=20)
+    # run hopp simulation
+    hi.simulate(project_life=20)
 
-if showplots:
+
+    if showplots == 'true':
     # show plot code
+        horizontal_plane = hi.calculate_horizontal_plane(x_resolution=200, y_resolution=100, height= 6.5)
+        fig, ax_list = plt.subplots(1, 1, figsize=(10, 8))
+        wakeviz.visualize_cut_plane(horizontal_plane, ax=ax_list, title= "Horizontal", clevels = 100, max_speed = 8)
+        wakeviz.show_plots()    
 
-if name == '__main__'
-    run_simulation(showplots= true)
+
+
+if __name__ == "__main__":
+    runsimulation(showplots= 'true')
+else:
+    runsimulation()
+
 
 
 
--- greenheart/simulation/technologies/dac/direct_air_capture.py
+++ greenheart/simulation/technologies/dac/direct_air_capture.py
@@ -1,100 +1,67 @@
-
-# Input = array with hourly timescale of wind speeds, m/s (assumes wind speed is the average over the hour)
-# This function uses the hourly wind speed data to find CO2 uptake (in tons of CO2 over that hour). 
-# Then, the power consumption over the hour is calculated based on the CO2 uptake using the conversion 7.2 GJ
-# or 2000 kWh per ton of CO2 adsorbed. 
-
-# This is a little janky because we have set the adsorption/ desorption times to 54 / 6  minutes 
-# regardless of how much CO2 is adsorbed during that hour. The total energy requirement should be about 
-# equal to the energy of the desorption stage. I'm not sure if a "6 minute desorption time" would 
-# require the same amount of energy no matter how much CO2 is being desorbed or not.
-# Each unit will be made of one base and then 6 modules, stacked on top of eachother. Each module is 
-# 1 meter tall. Theoretically, the models will have staggered adsorption/ desorption stages throughout the 
-# hour. Using 6 minutes as the desorption time and 6 modules, this means that every hour there are 36 minutes
-# where one of the modules is desorbing CO2 while the other 5 are each adsorbing. The remaining 24 minutes, 
-# all modules are adsorbing CO2 (no/ very low energy requirement). 
-# However, I propose that we assume each unit requires constant power throughout the hour for easier 
-# calculations, especially with the batteries/ intermittent wind power.
+# Input = 1D array with hourly timescale of wind speeds, m/s (assumes wind speed is the average over the hour)
 
 import numpy as np
-from scipy import interpolate
 
-def driver():
+def driver(windspeed, numDACs):
 
-    hourly_CO2_prod, total = calc_CO2_prod(hourly_data)
+    windspeed = np.array(windspeed)
+    windspeeds = windspeed
+    # Adding row to numpy array
+    heights = [2.5,3.5,4.5,5.5,6.5,7.5]
+    for _ in heights[0:-1]:
+        windspeeds = np.vstack ((windspeeds, windspeed))
 
-    hourly_energy_consumed = energyconsumptioncalc(hourly_CO2_prod)
+    k = 0    
+    for height in heights:
+        height_var = (height/10)**0.143
+        # v = v_ref * (z/z_ref)^0.143
+        windspeeds[k,:] = np.multiply(windspeeds[k,:],height_var)
+        k = k+1
 
-    return hourly_CO2_prod, total, hourly_energy_consumed
+    # CO2 produced per DAC module (ie. 1 kg sorbent)
+    CO2_permodule = CO2_produced(windspeeds)
 
+    # CO2 produced per hour per DAC unit (ie. 1 stack of six modules)
+    CO2_unit = np.sum(CO2_permodule, axis = 0)
 
-# make up series of hourly wind speeds for testing purposes 
-hourly_data = np.array([1.2,3.4,2.6,1.3,1.8,2.7,3.6,5.8,6.9,5.6])
+    # CO2 produced per hour per farm with numDACs DAC units (in moles CO2)
+    CO2_total = np.multiply(numDACs,CO2_unit)
 
-# find tons of CO2 produced over that hour (by one module) at the defined wind speed
-def calc_CO2_prod(hourly_data):
+    # tons of CO2 collected per hour
+    # mole CO2 to US ton conversion: 0.044 kg/mole, 907.185 kg/ton
+    convert = 0.044 / 907.185
+    CO2_total = np.multiply(CO2_total,convert)
 
-    # experimentally determined data (from Realff et al.)
+    # power input required per hour (kWh)
+    power_total = np.multiply(2000, CO2_total)
 
-    # wind speeds (m/s)
-    set_wind_speeds = np.array([0.01, 0.3, 1, 3, 5, 7])
+    return CO2_total, power_total
 
-    # CO2 adsorption (moles CO2 adsorbed per kg sorbent in 54 minutes)
-        # Note: desorption time set to 6 minutes (assume complete adsorption). No CO2 is adsorbed during 
-        # desoprtion stage, so the amount of CO2 adsorbed in 54 minutes represents the hourly total. 
-    set_molar_CO2 = np.array([0.04, 0.22, 0.60, 0.90, 0.935, 0.96])
+def CO2_produced(x):
+    # find CO2 uptake by 1 kg sorbent in one hour (specifically 54 minutes but with 6 minute desorption) at a wind speed x.
+    # the wind speed x is defined at the midpoint height of the individual DAC module. 
 
+    y = np.multiply(0.1503,np.log(x)) + 0.6377
+    return y
+
+    """""
+    # data from Lively et al. 
+    wind_speed = [0.1, 0.3, 1, 3, 5, 7]
+    # CO2 uptake in 54 minutes (moles CO2 / kg sorbent)
+    CO2_uptake = [0.04202532,0.24860759, 0.61721519, 0.86025316,0.92506329,0.95949367]
     # molar mass of CO2: kg/ mole
     mCO2 = 0.044
+    # convert moles CO2 to kg CO2
+    CO2_uptake = np.multiply(mCO2, CO2_uptake)
+    # convert kg CO2 to US tons CO2
+    CO2_uptake = np.multiply(0.00110231, CO2_uptake)
 
-    set_kg_CO2 = set_molar_CO2 * mCO2
+    tck = interpolate.splrep(wind_speed, CO2_uptake)
+    return interpolate.splev(x, tck)
+    """
 
-    CO2_prod = []
-
-    # at every reported wind speed, use an interpolation of the two surrounding data points to find the 
-    # hourly uptake of CO2 (in kg CO2 per kg sorbent, ie. per one module)
-    for speed in hourly_data:
-        if 0.01<= speed <= 0.3:
-           f = interpolate.interp1d(set_wind_speeds[0:2], set_kg_CO2[0:2])
-           CO2_prod.append(f(speed))
-        elif 0.3 < speed <= 1:
-            f = interpolate.interp1d(set_wind_speeds[1:3], set_kg_CO2[1:3])
-            CO2_prod.append(f(speed))
-        elif 1 < speed <= 3:
-            f = interpolate.interp1d(set_wind_speeds[2:4], set_kg_CO2[2:4])
-            CO2_prod.append(f(speed))
-        elif 3 < speed <= 5:
-            f = interpolate.interp1d(set_wind_speeds[3:5], set_kg_CO2[3:5])
-            CO2_prod.append(f(speed))
-        elif 5 < speed <= 7:
-            f = interpolate.interp1d(set_wind_speeds[4:6], set_kg_CO2[4:6])
-            CO2_prod.append(f(speed))
-        # if wind speed is greater than 7 m/s, assume that CO2 uptake is the same rate as wind speed at 7 m/s
-        elif speed >= 7:
-            CO2_prod.append(set_kg_CO2[5])
-
-    # convert to numpy array
-    hourly_CO2_prod = np.array(CO2_prod)
-
-    # convert CO2 produced from kg to tons
-    hourly_CO2_prod = (hourly_CO2_prod)/907.185 
-    
-    # sum over all hours
-    total = np.sum(hourly_CO2_prod)
-
-    return hourly_CO2_prod, total
-
-# Input = amount of CO2 adsorbed over an hour (tons). Output = energy consumed during that hour
-def energyconsumptioncalc(hourly_CO2_production):
-
-    # calculate energy consumed in kWh over that hour
-    hourly_energy_consumed = 2000*hourly_CO2_production
-    return hourly_energy_consumed
-
-# call main function and then print all of the outputs.
-
-hourly_CO2_prod, total, hourly_energy_consumed = driver()
-
-print(f"CO2 uptake per hour in timescale (in tons CO2): {hourly_CO2_prod}",end = "\n\n")
-print(f"total CO2 uptake over all hours: {total} tons CO2", end = "\n\n")
-print(f"the energy requirement (in kWh) each hour is: {hourly_energy_consumed}", end = "\n\n")
+# windarray = [1,4.3,2.8,7,6.5,5.2,8]
+# num_dac = 20
+# CO2_total, power_total = driver(windarray,num_dac)
+# print(CO2_total)
+# print(power_total)
